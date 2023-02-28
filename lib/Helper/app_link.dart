7830d32f3dceb401739e7a84a33fddc47848aa6f
Daily Programmer <bayumoonfang@gmail.com>
2023-02-28 10:56:33 +0700

    28022023

--- lib/Helper/app_link.dart
+++ lib/Helper/app_link.dart
@@ -1,2 +1,2 @@
-String applink = "https://dev.mishr.missolution.id/";
-//String applink = "https://mishr.missolution.id/";
+//String applink = "https://dev.mishr.missolution.id/";
+String applink = "https://mishr.missolution.id/";
