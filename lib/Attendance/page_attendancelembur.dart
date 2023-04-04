209d51f2d8f04d038eb424e39067f6b5327ec467
Ragil Bayu Respati <bayumoonfang@gmail.com>
2023-04-04 11:43:39 +0700

    05042023b

--- lib/Attendance/page_attendancelembur.dart
+++ lib/Attendance/page_attendancelembur.dart
@@ -141,13 +141,48 @@ class _PageClockInLembur extends State<PageClockInLembur> {
   }
 
 
+  _loaddata2() async {
+    await getAllCabang();
+    EasyLoading.show(status: AppHelper().loading_text);
+    _noteclockin.clear();
+    await getSettings();
+    await AppHelper().getConnect().then((value){
+      if(value == 'ConnInterupted'){
+        getBahasa.toString() == "1"?
+        AppHelper().showFlushBarsuccess(context, "Koneksi terputus...") :
+        AppHelper().showFlushBarsuccess(context, "Connection Interupted...");
+        setState(() {
+          isPressed = false;
+        });
+        return false;
+      } else {
+        setState(() {
+          isPressed = true;
+        });
+      }
+    });
+    await AppHelper().getSession().then((value){
+      setState(() {
+        if(value[0] == "" || value[0] == null) {
+          Navigator.pushReplacement(context, ExitPage(page: PageLogin(getBahasa, getToken)));
+          EasyLoading.dismiss();
+        }
+      });});
+    EasyLoading.dismiss();
+
+  }
+
+
+  String cabang_range = '...';
   getNewWorkLocation2(getLokasi) async {
     await AppHelper().getNewWorkLocation(getLokasi).then((value){
       setState(() {
         locationLat = value[0];
         locationLong = value[1];
+        cabang_range = value[2];
         _locationCabang = LatLng(double.parse(value[0]), double.parse(value[1]));
-        _loaddata();
+        _loaddata2();
+        rangemaxstr = cabang_range;
       });});
   }
 
@@ -521,7 +556,7 @@ class _PageClockInLembur extends State<PageClockInLembur> {
                     rotateGesturesEnabled : false,
                     circles: Set.from([Circle( circleId: CircleId('currentCircle'),
                       center: _locationCabang,
-                      radius: 60,
+                      radius: double.parse(rangemaxstr.toString()),
                       fillColor: Colors.blue.shade100.withOpacity(0.5),
                       strokeColor:  Colors.blue.shade100.withOpacity(0.1),
                     ),],),
